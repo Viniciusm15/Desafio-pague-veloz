dffcf7f32075a809e1e992eadfc5828231244195
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2026-07-07 12:48:01 -0300

    feat: add repository contracts for Account and Customer

---
+++ PagueVeloz.TransactionEngine/src/PagueVeloz.Domain/Interfaces/IAccountRepository.cs
@@ -0,0 +1,9 @@
+﻿using PagueVeloz.Domain.Entities;
+
+namespace PagueVeloz.Domain.Interfaces;
+
+public interface IAccountRepository
+{
+    Task<Account?> GetByIdAsync(Guid accountId);
+    Task AddAsync(Account account);
+}
---
+++ PagueVeloz.TransactionEngine/src/PagueVeloz.Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿using PagueVeloz.Domain.Entities;
+
+namespace PagueVeloz.Domain.Interfaces;
+
+public interface ICustomerRepository
+{
+    Task<Customer?> GetByIdAsync(Guid customerId);
+    Task AddAsync(Customer customer);
+}
