cb82977a444de67fc388627c42cf5f8ae6530302
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2026-07-07 15:03:42 -0300

    feat: add migration for AccountOperation and ReservedBalance

---
+++ PagueVeloz.TransactionEngine/src/PagueVeloz.Infrastructure/Migrations/20260707173457_AddAccountOperationAndReservedBalance.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PagueVeloz.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountOperationAndReservedBalance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ReservedBalance",
+                table: "Accounts",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.CreateTable(
+                name: "AccountOperations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    OccurredAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountOperations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AccountOperations_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountOperations_AccountId",
+                table: "AccountOperations",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountOperations");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ReservedBalance",
+                table: "Accounts",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)",
+                oldDefaultValue: 0m);
+        }
+    }
+}
